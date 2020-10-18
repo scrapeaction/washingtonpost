32ac1513865bc9b1a7473c1ecb2276cd99d7f79e
Kushal <kushaldeveloper@gmail.com>
2020-10-17 19:35:01 -0600

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -1,7 +1,28 @@
 'use strict';
 const puppeteer = require('puppeteer');
 
-crawlPage();
+crawlPage("https://washingtonpost.com", "frontpage");
+crawlPage("https://washingtonpost.com/todays_paper/updates/", "todays-papeer");
+crawlPage("https://washingtonpost.com/politics/", "politics");
+crawlPage("https://washingtonpost.com/opinions", "opinions");
+crawlPage("https://washingtonpost.com/national/investigations", "investigations");
+crawlPage("https://washingtonpost.com/business/technology", "technology");
+crawlPage("https://washingtonpost.com/world", "world");
+crawlPage("https://washingtonpost.com/dc", "dc");
+crawlPage("https://washingtonpost.com/sports", "sports");
+crawlPage("https://washingtonpost.com/race-america", "race-and-reckoning");
+crawlPage("https://washingtonpost.com/entertainment", "arts-and-entertainment");
+crawlPage("https://washingtonpost.com/business", "business");
+crawlPage("https://washingtonpost.com/personal-finance", "personal-finance");
+crawlPage("https://washingtonpost.com/climate-environment", "climante-and-environment");
+crawlPage("https://washingtonpost.com/elections", "elections");
+crawlPage("https://washingtonpost.com/immigration", "immigration");
+crawlPage("https://washingtonpost.com/transportation", "transportation");
+crawlPage("https://washingtonpost.com/travel", "travel");
+crawlPage("https://washingtonpost.com/video-games", "video-games");
+crawlPage("https://washingtonpost.com/outlook", "outlook");
+crawlPage("https://washingtonpost.com/outlook/five-myths", "five-myths");
+
 
 function delay(time) {
     return new Promise(function (resolve) {
@@ -9,7 +30,7 @@ function delay(time) {
     });
 }
 
-function crawlPage() {
+function crawlPage(url, prefix) {
     (async () => {
 
         const args = [
@@ -29,36 +50,45 @@ function crawlPage() {
             width: 1920,
             height: 1080
         });
-        
-        await page.goto("https://washingtonpost.com", {
+
+        await page.goto(url, {
             waitUntil: 'networkidle0',
             timeout: 0
         });
 
+        await page.screenshot({
+            path: `screenshots/${prefix}.png`,
+            fullPage: true
+        });
+        await page.screenshot({
+            path: `screenshots/${prefix}-fold.png`,
+            fullPage: false
+        });
+
         const addresses = await page.$$eval('a', as => as.map(a => a.href));
-
+        const padding = addresses.length % 10;
         for (let i = 0; i < addresses.length; i++) {
-            console.log(`Now serving ${i} of ${addresses.length}: ${addresses[i]}`);
             try {
-                await page.goto(addresses[i], { waitUntil: "networkidle0", timeout: 0 });
+                if (addresses[i].startsWith("http") === true) {
+                    console.log(`Now serving ${i} of ${addresses.length}: ${addresses[i]}`);
+                    await page.goto(addresses[i], { waitUntil: "networkidle0", timeout: 300000 });
 
-                const watchDog = page.waitForFunction(() => 'window.status === "ready"', { timeout: 0 });
-                await watchDog;
+                    const watchDog = page.waitForFunction(() => 'window.status === "ready"', { timeout: 300000 });
+                    await watchDog;
 
-                await delay(4000);
-                console.log(`waited for four seconds`);
-                await page.screenshot({
-                    path: `screenshots/screenshots-${i}.png`,
-                    fullPage: true
-                });
-                await page.screenshot({
-                    path: `screenshots/screenshots-${i}-fold.png`,
-                    fullPage: false
-                });
+                    await page.screenshot({
+                        path: `screenshots/${prefix}-${i.toString().padStart(padding, '0')}.png`,
+                        fullPage: true
+                    });
+                    await page.screenshot({
+                        path: `screenshots/${prefix}-${i.toString().padStart(padding, '0')}-fold.png`,
+                        fullPage: false
+                    });
+                }
             } catch (error) {
                 console.error(error);
             } finally {
-                console.log(`Finished serving ${i} of ${addresses.length}: ${addresses[i]}`);
+                console.log(`Finished with ${i} of ${addresses.length}: ${addresses[i]}`);
             };
         }
 
